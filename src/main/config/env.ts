9a15e966b962d7348b0b236769467396cf217ccb
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2021-09-23 17:51:46 -0300

    refactor: change database name

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/crud-clean-architecture',
   port: process.env.PORT ?? 3333
 }
